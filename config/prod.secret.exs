58bb96fd3525f7c6e249acbc46fe5c0ddf95ca31
shibani <shibani@8thlight.com>
2018-09-01 16:06:43 -0400

    updating production vars

--- config/prod.secret.exs
+++
@@ -1,7 +0,0 @@
-use Mix.Config
-
-# In this file, we keep production configuration that
-# you likely want to automate and keep it away from
-# your version control system.
-config :hello_phoenix_heroku, HelloPhoenixHeroku.Endpoint,
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
